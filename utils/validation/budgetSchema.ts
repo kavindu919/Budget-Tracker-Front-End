f555708a7b510aa0b084342c565f194daa3d776f
kavindu919 <jayakodyk2@gmail.com>
2026-03-09 19:56:12 +0530

    feat: create budget schema

---
+++ utils/validation/budgetSchema.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const budgetSchema = z.object({
+  id: z.string(),
+  amount: z.coerce
+    .number({ message: "Amount is required" })
+    .positive("Amount must be positive"),
+  period: z.enum(["daily", "weekly", "monthly", "yearly"]),
+  alertLimit: z
+    .union([
+      z.literal("").transform(() => undefined),
+      z.coerce.number().min(0, {
+        message: "Alert limit must be 0 or greater",
+      }),
+    ])
+    .optional(),
+  categoryId: z.string().min(1, "Category is required"),
+});
